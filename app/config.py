d87a6642fba54b1c0d08579c7244a2908b9ad5d0
Rewdog <rew@rewdog.com>
2020-08-29 18:33:01 -0400

    Initial commit of flask app

---
+++ app/config.py
@@ -0,0 +1,15 @@
+class World:
+  def __init__(self, name, region, portsuffix):
+    self.name = name
+    self.region = region
+    self.portsuffix = portsuffix
+
+GOOGLEMEETURL = "https://meet.google.com/ex-amp-le"
+STARTAPIKEY = 'APIKEYFORLAMBDATOSTARTINSTANCE'
+LAMBDABASEPATH = 'https://locationoflambdastarter.amazonaws.com/default/'
+STATUSBASEPATH = 'https://locationoflambdastatus.amazonaws.com/default/'
+TITLE = "Pillager's Minecraft Server Status"
+DOMAIN = "example.com"
+SERVERONE = World(name="World One", region="worldone", portsuffix="5")
+SERVERTWO = World(name="World Two", region="worldtwo", portsuffix="6")
+SERVERTHREE = World(name="World Three", region="worldthree", portsuffix="7")
